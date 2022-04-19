45b1329c63c0e034bf2528e487e7b23313021b60
Iker Toscano <ikerismak@gmail.com>
2022-04-19 16:31:10 -0500

    adding class and metods lesson

--- weekly_mission_2/examples/2_clases_y_objetos.js
+++ weekly_mission_2/examples/2_clases_y_objetos.js
@@ -178,13 +178,173 @@ console.log(woopa.getExercisesCompleted)
 
 
 
-
-
-
-
 console.log("");
 console.log("#------Ejemplo 7: Setters para modificar los atributos del objeto------#");
 console.log("");
+
+
+
+class MissionCommander {
+
+    constructor(name,mission){
+
+        this.name = name
+        this.mission = mission
+        this.students = 0 
+        this.lives = 0
+
+    }
+
+
+    get getStudents(){
+
+        return this.students
+    }
+
+    get getLives(){
+        
+        return this.lives
+    }
+    
+    set setStudents(students){
+
+        this.students = students
+    }
+
+    set setLives(lives){
+        this.lives = lives
+      }
+
+}
+
+
+console.log("Ejemplo 7: Setters para modificar los atributos del objeto")
+
+
+
+const missionCommanderNode = new MissionCommander("Carlo", "NodeJS")
+
+console.log(missionCommanderNode.getStudents) // 0 por default
+console.log(missionCommanderNode.getLives)// 0 por default
+
+console.log("Ejemplo 7: actualizamos los atributos por medio de los setters")
+
+// actualizamos los atributos por medio de los setters
+missionCommanderNode.setStudents = 100
+missionCommanderNode.setLives = 3
+
+console.log(missionCommanderNode.getStudents) // 0 por default
+console.log(missionCommanderNode.getLives)// 0 por default
+
+
+
+
+
+
+
+
+
 console.log("");
 console.log("#------Ejemplo 8: Métodos static nos ayudan a escribir métodos en una clase que podemos usar sin necesidad de instanciar algún objeto------#");
 console.log("");
+
+
+
+
+class Toolbox {
+
+    static getMostUsefulTools(){
+
+        return ["command line", "git", "Text Editor"]
+    }
+}
+
+
+
+console.log("Ejemplo 8: Métodos static")
+// Puedo llamar el método static directamente con el nombre de la clase
+console.log(Toolbox.getMostUsefulTools())
+// Si intentamos instanciar un objeto, no podremos llamar este método static
+//const toolbox = new Toolbox()
+//console.log(toolbox.getMostUsefulTools()) // is not a function
+
+
+
+
+console.log("");
+console.log("#------ HERENCIA: Nos permite relacionar clases entre sí y rehusar sus componentes ------#");
+console.log("");
+
+console.log("");
+console.log("#------Ejemplo  9: Herencia entre dos clases ------#");
+console.log("");
+
+
+class Developer {
+    constructor(name, mainLang, stack){
+      this.name =  name
+      this.mainLang = mainLang
+      this.stack = stack
+    }
+  
+    get getName() {
+          return this.name
+      }
+  }
+
+  console.log("Ejemplo  9: Herencia entre dos clases")
+  const carloDev = new Developer("Carlo", "js", ["elixir", "groovy", "lisp"])
+  console.log(carloDev)
+
+
+// Se usa la palabra extends para indicar que heredarás las propiedades de la clase Padre (Developer) en la clase definida.
+// Podemos definir una clase vacía y rehusar todos los componentes de la clase padre
+
+
+class LaunchXStudent extends Developer{
+}
+
+const carloLaunchXDev = new LaunchXStudent("Carlo", "js", ["elixir", "groovy", "lisp"])
+console.log(carloLaunchXDev)
+console.log(carloLaunchXDev.getName)
+
+
+
+console.log("");
+console.log("#------ Ejemplo 10: Overrinding methods ------#");
+console.log("");
+
+
+
+class Explorer{
+    constructor(name, username, mission){
+     this.name = name
+     this.username = username
+     this.mission = mission
+    }
+  
+    getNameAndUsername(){
+     return `Explorer ${this. name}, username: ${this.username}`
+    }
+  }
+
+
+  class Viajero extends Explorer {
+    constructor(name, username, mission, cycle){
+      super(name, username, mission) // SUPER nos ayudará a llamar el constructor padre
+      this.cycle = cycle // Agregamos este atributo de la clase Viajero, es exclusiva de esta clase y no de la clase padre
+    }
+  
+    getGeneralInfo(){
+      let nameAndUsername = this.getNameAndUsername() // llamamos el método de la clase padre
+      // nameAndUsername  es una variable de esta función, no necesitas usar this para referenciarla.
+      return `${nameAndUsername}, Ciclo ${this.cycle}`
+    }
+  }
+
+
+const viajero1 = new Viajero("Carlo", "LaunchX", "Node JS", "Abril 2022")
+    console.log("Ejemplo 10: Overrinding methods")
+    console.log(viajero1)
+    console.log(viajero1.getNameAndUsername()) // Método de la clase padre
+    console.log(viajero1.getGeneralInfo()) // Método de la clase hija
