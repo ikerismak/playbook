0bb8f4e2cba72c862b52b60726a571d29d6f8e73
Iker Toscano <ikerismak@gmail.com>
2022-04-18 19:48:17 -0500

    adding new excesices

---
+++ weekly_mission_2/ejercicios/ejercicio1/github.js
@@ -0,0 +1,52 @@
+const repo = {
+
+    name: "reponame",
+    author: "iker",
+    lenguage: "python",
+    numberOfCommits: 100,
+    stars: 199,
+    forks: 299,
+    issuesOpen: 10,
+    issuesClose: 10,
+
+    getTotalIssues: function(){
+
+        return this.issuesOpen + this.issuesClose
+    },
+
+
+    getGeneralInfo: function(){
+
+        return `This repository ${this.name} was created by ${this.author}`
+    }
+
+};
+
+
+
+console.log("Nombre del repo:" + repo.name)
+console.log("Issues totales: " + repo.getTotalIssues())
+console.log(repo.getGeneralInfo())
+
+
+
+
+const Issue = {
+
+    title:"non complete",
+    repositoryNameAssociated: "reponame",
+    status: "down",
+    numberOfComments: 10,
+    labels: 20,
+    author: "pepe",
+    dateCreated: "10-10-2020",
+    lastUpdated: "10-10-2022",
+
+    getTitleAndAuthor: function(){
+        return `we have this author ${this.author} for this title ${this.title}`
+    },
+
+    getGeneralInfo: function(){
+        return `we have this author ${this.author} for this title ${this.title}`
+    }
+}
