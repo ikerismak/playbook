8a0a29d7cba37fd1419db8336afe9f4e393d4d18
Iker Toscano <ikerismak@gmail.com>
2022-04-20 18:40:47 -0500

    finish ejerccio 1

---
+++ weekly_mission_2/ejercicios/ejercicio1/twitter.js
@@ -0,0 +1,53 @@
+const user = {
+
+
+    userName : "ikerismak",
+    name : "iker",
+    bio: "de colima para el mundo",
+    age: 23
+
+
+}
+
+
+
+const trending_topic = {
+
+    catogory:"movies",
+    title:"the oscars best movie ",
+    userintipic: "the movieland",
+    likes: 23,
+    retwits: 20,
+
+}
+
+
+
+const hashtag = {
+
+
+    user:"iker toscano",
+    country: "mexico",
+    content: "the best actor",
+    title: "@holaperros"
+
+
+
+
+
+    
+
+
+}
+
+console.log("");
+console.log("--USER--");
+console.log("");
+console.log(user);
+console.log("--HASHTAG OBJ--");
+console.log("");
+console.log(hashtag);
+console.log("");
+console.log("--TRENDING TOPIC--");
+console.log("");
+console.log(trending_topic);
---
+++ weekly_mission_2/examples/diferencia_objeto_clase.js
@@ -0,0 +1,44 @@
+console.log("----------POO-----------");
+console.log("");
+
+
+
+//objeto de JS
+const woopaJs ={
+
+    name: "woopa",
+    mission: "noode js",
+    age: 12,
+    color: "pink"
+}
+
+
+
+console.log("");
+console.log("-------CREAR UNA CLASE---------");
+console.log("");
+
+
+
+class Ajolonauta{
+
+
+}
+
+//Instanciar un objeto de una clase
+
+const woopa = new Ajolonauta()
+
+const woopa2 = new Ajolonauta()
+
+
+
+
+console.log("---Este es un objeto de JS");
+console.log("");
+console.log(WoopaJs);
+console.log("---Este es un objeto instanciado de una clase en este caso Ajolonauta");
+console.log("");
+console.log(Woopa);
+
+
